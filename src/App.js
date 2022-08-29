8de7aedcb1d0a400a436d58d33074bbc633c7ddf
bhubkyle <bhubkylegarcia@gmail.com>
2022-08-29 22:24:09 +0800

    sample layout

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import {useState,useRef} from 'react'
 import Login from "./pages/login";
+import { BrowserRouter, BrowserRouter as Router, Route,Routes } from 'react-router-dom'
+import Home from './pages/home';
 
 function App() {
-  const refContainer = useRef(null)
-  const [user,setUser] = useState('')
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    setUser(refContainer.current.value)
-   
-  }
-   console.log(user)
+  
   return (
+    <BrowserRouter>
+    <Routes>
+    <Route path="/" element={<Home/>} />
+    <Route path="/login" element={<Login/>} />
+    </Routes>
+    </BrowserRouter>
     
-    <div className="App">
-      <Login />
-
-    
-    </div>
   );
 }
 
